93e82c045c57791fea0de05600e940af4fe4792b
Syer10 <syer10@users.noreply.github.com>
2022-12-15 17:46:14 -0500

    Remove withJava()

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,6 @@ kotlin {
         compilations.all {
             kotlinOptions.jvmTarget = "1.8"
         }
-        withJava()
         testRuns["test"].executionTask.configure {
             useJUnitPlatform()
         }
